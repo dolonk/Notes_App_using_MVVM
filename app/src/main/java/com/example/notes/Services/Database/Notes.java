3e5ba556fb84e3d8f3f89911535d007bb420be29
dolonk9 <dolonk9@gmail.com>
2022-12-26 00:09:12 +0600

    Initial Statement

---
+++ app/src/main/java/com/example/notes/Services/Database/Notes.java
@@ -0,0 +1,27 @@
+package com.example.notes.Services.Database;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "Notes_Database")
+public class Notes {
+
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    @ColumnInfo(name = "Notes_tittle")
+    public String notesTittle;
+
+    @ColumnInfo(name = "Notes_subTittle")
+    public String notesSubTittle;
+
+    @ColumnInfo(name = "Notes_date")
+    public String notesDate;
+
+    @ColumnInfo(name = "Notes")
+    public String notes;
+
+    @ColumnInfo(name = "Notes_priority")
+    public String NotesPriority;
+}
