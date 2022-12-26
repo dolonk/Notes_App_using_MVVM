f281a282376a1d6f7f2aa293a2b0e3f501a46e1b
dolonk9 <dolonk9@gmail.com>
2022-12-26 11:08:05 +0600

    Initial Notes Repository

---
+++ app/src/main/java/com/example/notes/Services/Repository/NotesRepository.java
@@ -0,0 +1,35 @@
+package com.example.notes.Services.Repository;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import com.example.notes.Services.Database.NotesDao;
+import com.example.notes.Services.Database.NotesDatabase;
+import com.example.notes.Services.Model.Notes;
+
+import java.util.List;
+
+public class NotesRepository {
+
+    public NotesDao notesDao;
+    public LiveData<List<Notes>> getAllNotes;
+
+    public NotesRepository(Application application) {
+        NotesDatabase database = NotesDatabase.getDatabaseInstance(application);
+        notesDao = database.notesDao();
+        getAllNotes = notesDao.getAllNotes();
+    }
+
+    void insertNotes(Notes notes) {
+        notesDao.insertNotes(notes);
+    }
+
+    void deleteNotes(int id) {
+        notesDao.deleteNotes(id);
+    }
+
+    void updateNotes(Notes notes) {
+        notesDao.updateNotes(notes);
+    }
+}
