812fc116475de64fe7030aae2f7934bf0f5a20f1
dolonk9 <dolonk9@gmail.com>
2022-12-26 12:36:00 +0600

    Initial Notes LiveData

---
+++ app/src/main/java/com/example/notes/ViewModel/NotesViewModel.java
@@ -0,0 +1,36 @@
+package com.example.notes.ViewModel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.notes.Services.Model.Notes;
+import com.example.notes.Services.Repository.NotesRepository;
+
+import java.util.List;
+
+public class NotesViewModel extends AndroidViewModel {
+    public NotesRepository repository;
+    public LiveData<List<Notes>> getAllNotes;
+
+    public NotesViewModel(@NonNull Application application) {
+        super(application);
+
+        repository = new NotesRepository(application);
+        getAllNotes = repository.getAllNotes;
+    }
+
+    void insertNote(Notes notes) {
+        repository.insertNotes(notes);
+    }
+
+    void deleteNote(int id) {
+        repository.deleteNotes(id);
+    }
+
+    void updateNote(Notes notes) {
+        repository.updateNotes(notes);
+    }
+}
