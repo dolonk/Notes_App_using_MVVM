2f38d0c0c24e276f7e3414357270fa36d5061b67
dolonk9 <dolonk9@gmail.com>
2023-01-02 16:51:09 +0600

    Add required function and change app icon

--- app/src/main/java/com/example/notes/Model/Ui/InsertNoteActivity.java
+++ app/src/main/java/com/example/notes/Model/Ui/InsertNoteActivity.java
@@ -3,7 +3,9 @@ package com.example.notes.Model.Ui;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Toast;
@@ -52,17 +54,30 @@ public class InsertNoteActivity extends AppCompatActivity {
         });
 
         binding.doneFloatingActionButton.setOnClickListener(v -> {
+
             tittle = binding.tittleID.getText().toString();
             subTittle = binding.subTittleID.getText().toString();
             notes = binding.noteID.getText().toString();
-            CreatedNotes( tittle, subTittle, notes);
+
+            if (TextUtils.isEmpty(tittle)) {
+                Toast.makeText(InsertNoteActivity.this, "Please Enter Tittle Name", Toast.LENGTH_SHORT).show();
+                binding.tittleID.setError("Tittle is requirement");
+            } else if (TextUtils.isEmpty(notes)) {
+                Toast.makeText(InsertNoteActivity.this, "Please Enter notes data", Toast.LENGTH_SHORT).show();
+                binding.noteID.setError("Note is Required");
+                binding.noteID.requestFocus();
+            } else {
+                CreatedNotes(tittle, subTittle, notes);
+            }
         });
+
+
     }
 
     private void CreatedNotes(String tittle, String subTittle, String notes) {
 
         Date date = new Date();
-        CharSequence sequence = DateFormat.format("d MMMM,yyyy",date.getTime());
+        CharSequence sequence = DateFormat.format("d MMMM,yyyy", date.getTime());
 
         Notes insertNotes = new Notes();
         insertNotes.notesTittle = tittle;
